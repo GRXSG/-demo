214b5daf0e0254e592934369baf6a9088412ecbc
GRXSG <2277243795@qq.com>
2023-01-06 19:37:01 +0800

    吏用Spring Cache缓存套餐数据

--- src/main/java/top/wennyy/controller/SetmealController.java
+++ src/main/java/top/wennyy/controller/SetmealController.java
@@ -2,16 +2,17 @@ package top.wennyy.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 import top.wennyy.common.R;
 import top.wennyy.dto.SetmealDto;
 import top.wennyy.entity.Category;
-import top.wennyy.entity.Dish;
 import top.wennyy.entity.Setmeal;
-import top.wennyy.entity.SetmealDish;
 import top.wennyy.service.CategoryService;
 import top.wennyy.service.SetmealDishService;
 import top.wennyy.service.SetmealService;
@@ -20,63 +21,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @Author HanWen
- * @Date 2022/12/12 12:40
- * @Version 1.0
  * 套餐管理
  */
+
 @RestController
 @RequestMapping("/setmeal")
 @Slf4j
 public class SetmealController {
+
     @Autowired
     private SetmealService setmealService;
 
     @Autowired
-    private SetmealDishService setmealDishService;
-
-    @Autowired
     private CategoryService categoryService;
 
-
-    /**
-     * 套餐分页管理
-     * @param page
-     * @param pageSize
-     * @param name
-     * @return
-     */
-    @GetMapping("/page")
-    public R<Page> page(int page, int pageSize, String name){
-        //分页构造器
-        Page<Setmeal> pageinfo = new Page<>(page, pageSize);
-        Page<SetmealDto> dtoPage = new Page<>();
-
-        //条件构造器
-        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
-        //添加过滤条件
-        queryWrapper.like(name != null,Setmeal::getName,name);
-        //添加排序条件，根据价格进行排序
-        queryWrapper.orderByAsc(Setmeal::getPrice);
-        //把查询出来的数据进行分页
-        setmealService.page(pageinfo,queryWrapper);
-
-        BeanUtils.copyProperties(pageinfo,dtoPage,"records");
-        List<Setmeal> records = pageinfo.getRecords();
-        List<SetmealDto> list=records.stream().map((item)->{
-            SetmealDto setmealDto = new SetmealDto();
-            BeanUtils.copyProperties(item,setmealDto);
-            Long categoryId = item.getCategoryId();
-            Category category = categoryService.getById(categoryId);
-            if (category != null){
-                String setmealDishName = category.getName();
-                setmealDto.setCategoryName(setmealDishName);
-            }
-            return setmealDto;
-        }).collect(Collectors.toList());
-        dtoPage.setRecords(list);
-        return R.success(dtoPage);
-    }
+    @Autowired
+    private SetmealDishService setmealDishService;
 
     /**
      * 新增套餐
@@ -84,11 +44,59 @@ public class SetmealController {
      * @return
      */
     @PostMapping
- public R<String> save(@RequestBody SetmealDto setmealDto){
+    @CacheEvict(value = "setmealCache",allEntries = true)
+    public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("套餐信息：{}",setmealDto);
+
         setmealService.saveWithDish(setmealDto);
+
         return R.success("新增套餐成功");
- }
+    }
+
+    /**
+     * 套餐分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name){
+        //分页构造器对象
+        Page<Setmeal> pageInfo = new Page<>(page,pageSize);
+        Page<SetmealDto> dtoPage = new Page<>();
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        //添加查询条件，根据name进行like模糊查询
+        queryWrapper.like(name != null,Setmeal::getName,name);
+        //添加排序条件，根据更新时间降序排列
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        setmealService.page(pageInfo,queryWrapper);
+
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,dtoPage,"records");
+        List<Setmeal> records = pageInfo.getRecords();
+
+        List<SetmealDto> list = records.stream().map((item) -> {
+            SetmealDto setmealDto = new SetmealDto();
+            //对象拷贝
+            BeanUtils.copyProperties(item,setmealDto);
+            //分类id
+            Long categoryId = item.getCategoryId();
+            //根据分类id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if(category != null){
+                //分类名称
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        dtoPage.setRecords(list);
+        return R.success(dtoPage);
+    }
 
     /**
      * 删除套餐
@@ -96,10 +104,30 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
         log.info("ids:{}",ids);
+
         setmealService.removeWithDish(ids);
+
         return R.success("套餐数据删除成功");
     }
 
+    /**
+     * 根据条件查询套餐数据
+     * @param setmeal
+     * @return
+     */
+    @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId + '_' + #setmeal.status")
+    public R<List<Setmeal>> list(Setmeal setmeal){
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(setmeal.getCategoryId() != null,Setmeal::getCategoryId,setmeal.getCategoryId());
+        queryWrapper.eq(setmeal.getStatus() != null,Setmeal::getStatus,setmeal.getStatus());
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        List<Setmeal> list = setmealService.list(queryWrapper);
+
+        return R.success(list);
+    }
 }
